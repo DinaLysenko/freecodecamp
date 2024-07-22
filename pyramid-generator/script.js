e75b4d642bdcd4e72f758eed19aec77a08549141
DinaLysenko <dinafate@rambler.ru>
2024-07-22 23:36:43 +0500

    for, for of

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -1,6 +1,11 @@
 const character = "#";
 const count = 8;
 const rows = [];
-for (let i = 0; i < count; "iteration") {
-
+for (let i = 0; i < count; i=i+1) {
+    rows.push(character.repeat(i+1));
+}
+let result=''
+console.log(result)
+for (const row of rows) {
+    result = result + "\n"+ row;
 }
